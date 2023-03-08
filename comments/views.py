310a276beb3b5ccdb757b8116498d875f35be928
Mts-Carneiro <mtscrn21@gmail.comgit config --global user.email mtscrn21@gmail.com>
2023-03-08 10:40:45 -0300

    feat: Create comments serializer and views

--- comments/views.py
+++ comments/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Comment
+from .serializer import CommentsSerializer
 
-# Create your views here.
+
+class CommentsView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentsSerializer
+
+    def perform_create(self, serializer):
+        return serializer.save(
+            user_id=self.request.user.id, post_id=self.kwargs.get("post_id")
+        )
