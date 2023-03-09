fa6efe23a2bfcbe198710caa6dc6166b2c5d8e56
JonathanVCB <viniciusjonathan@hotmail.com>
2023-03-09 00:03:15 -0300

    add create, update and destroy views for users

--- users/views.py
+++ users/views.py
@@ -6,3 +6,10 @@ from .serializers import UserSerializer
 class UserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    lookup_url_kwarg = "user_id"
